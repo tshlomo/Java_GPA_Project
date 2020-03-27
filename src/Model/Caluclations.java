9e11d74f70e5e5196cb5a93a35bc7def7d91abd3
SagiDardik <sgdardik@gmail.com>
2020-03-27 19:43:37 +0300

    +connection to db class + class with methods - add grade,delete grade,get all grades. +interface

--- src/Model/Caluclations.java
+++ src/Model/Caluclations.java
@@ -11,42 +11,5 @@ public class Caluclations {
         int score= testGrade*(testPercent/100)+hwGrade*(hwPercent/100);
         return score;
     }
-
-    public static int calcu_gpa() throws SQLException {
-        Statement statement = null;
-        ResultSet rs = null;
-        double credits_sum = 0;
-        double grades_sum = 0;
-        Connection conn = DBconnection.GetDBConnection();
-        try {
-            statement = conn.createStatement();
-            rs = statement.executeQuery("SELECT finalGrade,credits FROM gpa");
-            while (rs.next()) {
-                grades_sum += rs.getInt("finalGrade") * rs.getDouble("credits");
-                credits_sum += rs.getDouble("credits");
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (statement != null) try {
-                statement.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            if (rs != null) try {
-                rs.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            if (conn != null) try {
-                conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-        return (int) (grades_sum / credits_sum);
-    }
     }
 
--- src/Model/DBActions.java
+++ src/Model/DBActions.java
@@ -129,6 +129,43 @@ public class DBActions {
 
         }
     }
+
+    public static int calcu_gpa() throws SQLException {
+        Statement statement = null;
+        ResultSet rs = null;
+        double credits_sum = 0;
+        double grades_sum = 0;
+        Connection conn = DBconnection.GetDBConnection();
+        try {
+            statement = conn.createStatement();
+            rs = statement.executeQuery("SELECT finalGrade,credits FROM gpa");
+            while (rs.next()) {
+                grades_sum += rs.getInt("finalGrade") * rs.getDouble("credits");
+                credits_sum += rs.getDouble("credits");
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) try {
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (conn != null) try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return (int) (grades_sum / credits_sum);
+    }
 }
 
 
