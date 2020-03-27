fe78a8c4f8c9d90ddee96d49cafa86cd50bdcb2a
tomer <tshlomo1@gmail.com>
2020-03-28 02:55:37 +0300

    Integer and not int

--- src/Model/Caluclations.java
+++ src/Model/Caluclations.java
@@ -10,27 +10,27 @@ import java.util.List;
 public class Caluclations {
 
     //func calculates and returns final grade based on final test score and percentage + hw score and percentage
-    public int calculate_Final_Grades(int testGrade, int testPercent, int hwGrade, int hwPercent){
-        int score= testGrade*(testPercent/100)+hwGrade*(hwPercent/100);
+    public Integer calculate_Final_Grade(Integer testGrade, Integer testPercent, Integer hwGrade, Integer hwPercent){
+        Integer score= testGrade*(testPercent/100)+hwGrade*(hwPercent/100);
         return score;
     }
 
 
     //func receives grades array + correspondent credits array and calculates gpa
     public Integer calculate_GPA() throws SQLException {
-    Double credits_sum=0.0;
-    Double grades_sum=0.0;
-    Integer i=0;
-    DBActions dbActions = new DBActions();
-    List<Integer> grades = dbActions.getFinalGrades();
-    List<Double>credits=dbActions.getCredits();
+        Double credits_sum=0.0;
+        Double grades_sum=0.0;
+        Integer i=0;
+        DBActions dbActions = new DBActions();
+        List<Integer> grades = dbActions.getFinalGrades();
+        List<Double>credits=dbActions.getCredits();
 
-    for (Integer x:grades) {
-        grades_sum+=x*credits.get(i);
-        credits_sum+=credits.get(i);
-        i++;
+        for (Integer x:grades) {
+            grades_sum+=x*credits.get(i);
+            credits_sum+=credits.get(i);
+            i++;
+        }
+        return (int) (grades_sum/credits_sum);
     }
-    return (int) (grades_sum/credits_sum);
 }
-    }
 
