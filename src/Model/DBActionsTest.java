16ec93b4d5511739da7e807fb53c8f9ce5fc1cbc
sagi <swaggyddd@gmail.com>
2020-03-30 23:01:40 +0300

    tests for DBActions

--- src/Model/DBActionsTest.java
+++ src/Model/DBActionsTest.java
@@ -12,20 +12,39 @@ class DBActionsTest {
     ResultSet rs = null;
     Connection conn = null;
 
-//    @org.junit.jupiter.api.Test
-//    void addGrade() throws SQLException {
-//        DBActions dbact = new DBActions();
-//        conn = DBconnection.getDBConnection();
-//        statement = conn.createStatement();
-//        try {
-//            dbact.addGrade("africa in black and white", 3, 75, 3.5, 90);
-//            rs = statement.executeQuery("SELECT * FROM GPA WHERE course='africa in black and white'");
-//
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @org.junit.jupiter.api.Test
+    void addGrade() throws SQLException {
+        Integer i=0;
+        Object [] array={3,75,90,2.0,99};
+        DBActions dbact = new DBActions();
+        conn = DBconnection.getDBConnection();
+        statement = conn.createStatement();
+        try {
+            dbact.addGrade("snowboarding", 3, 75, 90, 2.0,99); // adding same param values like in the array for comparing //
+            rs = statement.executeQuery("SELECT * FROM GPA WHERE course='snowboarding'");
+            while(rs.next()){
+                assertEquals(array[i],rs.getObject(i+2));
+            }
+            dbact.deleteGrade("snowboarding");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (statement != null) try {
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            conn = null;
+            rs = null;
+            statement = null;
+
+        }
+    }
 
     @org.junit.jupiter.api.Test
     void deleteGrade() throws SQLException {
@@ -33,9 +52,9 @@ class DBActionsTest {
         conn = DBconnection.getDBConnection();
         statement = conn.createStatement();
         try {
-            statement.execute("INSERT INTO GPA VALUES ('snowboarding',3,3,70,2.5,90)");
-            dbact.deleteGrade("snowboarding");
-            rs = statement.executeQuery("SELECT count(*) FROM GPA WHERE course=('snowboarding')");
+            statement.execute("INSERT INTO GPA VALUES ('ski',3,3,70,2.5,90)");
+            dbact.deleteGrade("ski");
+            rs = statement.executeQuery("SELECT count(*) FROM GPA WHERE course=('ski')");
             rs.next();
             assertEquals(0, rs.getInt(1));
 
@@ -64,11 +83,12 @@ class DBActionsTest {
             conn = DBconnection.getDBConnection();
             statement = conn.createStatement();
             try {
-                statement.execute("INSERT INTO GPA VALUES ('ski',3,3,70,2.5,90)");
-                dbact.editGrade("ski", 3, 3, 70, 2.5, 85);
-                rs = statement.executeQuery("SELECT finalGrade FROM GPA WHERE course=('ski')");
+                statement.execute("INSERT INTO GPA VALUES ('scala',3,3,70,2.5,90)");
+                dbact.editGrade("scala", 3, 3, 70, 2.5, 85);  // updating the finalGrade param //
+                rs = statement.executeQuery("SELECT finalGrade FROM GPA WHERE course=('scala')");
                 rs.next();
-                dbact.deleteGrade("ski");
+                assertEquals(85,rs.getDouble("finalGrade"));
+                dbact.deleteGrade("scala");
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
@@ -95,11 +115,11 @@ class DBActionsTest {
             conn = DBconnection.getDBConnection();
             statement = conn.createStatement();
             try {
-                statement.execute("INSERT INTO GPA VALUES ('ski',3,3,70,2.5,90)");
-                rs = statement.executeQuery("SELECT finalGrade FROM GPA WHERE course=('ski')");
+                statement.execute("INSERT INTO GPA VALUES ('groovy',3,3,70,2.5,90)");
+                rs = statement.executeQuery("SELECT finalGrade FROM GPA WHERE course=('groovy')");
                 rs.next();
                 assertEquals(90, rs.getInt("finalGrade"));
-                dbact.deleteGrade("ski");
+                dbact.deleteGrade("groovy");
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -119,4 +139,35 @@ class DBActionsTest {
                 statement = null;
             }
         }
+
+    @org.junit.jupiter.api.Test
+    void getCredit () throws SQLException {
+        DBActions dbact = new DBActions();
+        conn = DBconnection.getDBConnection();
+        statement = conn.createStatement();
+        try {
+            statement.execute("INSERT INTO GPA VALUES ('africa in black and white',3,3,70,4.0,90)");
+            rs = statement.executeQuery("SELECT credits FROM GPA WHERE course=('africa in black and white')");
+            rs.next();
+            assertEquals(4.0, rs.getDouble("credits"));
+            dbact.deleteGrade("africa in black and white");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) try {
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (rs != null) try {
+                rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            conn = null;
+            rs = null;
+            statement = null;
+        }
+    }
    }
