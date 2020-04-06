79c2177701296d0f875e3e3c5e59b4267d4e6a14
sagi <swaggyddd@gmail.com>
2020-04-06 19:23:45 +0300

    added @after in CalculationsTest

--- src/il/ac/hit/Test/JUnit/CalculationsTest.java
+++ src/il/ac/hit/Test/JUnit/CalculationsTest.java
@@ -1,17 +1,27 @@
 package il.ac.hit.Test.JUnit;
 
 import il.ac.hit.Model.Calculations;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculationsTest {
 
+    Calculations calc = new Calculations();
+
+
     @Test
     void calculate_Final_Grade() {
-        Calculations calc = new Calculations();
         Integer finalGrade = calc.calculate_Final_Grade(90.0, 70.0, 80.0, 30.0);
         assertEquals(87, finalGrade);
+        finalGrade=null;
+    }
+
+    @AfterClass
+    void afterClass(){
+        calc=null;
     }
 
 }
