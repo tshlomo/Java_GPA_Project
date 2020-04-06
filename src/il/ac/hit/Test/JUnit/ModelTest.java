160ab28a76c45ee2846fba0089f4c7d699c3da71
sagi <swaggyddd@gmail.com>
2020-04-06 21:37:44 +0300

    model test changes

--- src/il/ac/hit/Test/JUnit/ModelTest.java
+++ src/il/ac/hit/Test/JUnit/ModelTest.java
@@ -51,17 +51,15 @@ class ModelTest {
 
     @org.junit.jupiter.api.Test
     void addGrade() throws DBActionsException {
-        CourseDetails courseDetails = new CourseDetails("snowboarding", 3, 75, 90, 2.0, 99);
+        CourseDetails courseDetails;
         CourseDetails addedCourse = new CourseDetails("snowboarding", 3, 75, 90, 2.0, 99);
         try {
             statement = conn.createStatement();
             dbAct.addGrade(addedCourse); // adding same param values like in the array for comparing //
-            rs = statement.executeQuery("SELECT * FROM GPA WHERE course='snowboarding'");
-            while (rs.next()) {
-                assertTrue(courseDetails.isSameCourse(addedCourse));
-            }
+            courseDetails=dbAct.getCourse("snowboarding");
+            assertTrue(courseDetails.isSameCourse(addedCourse));
         } catch (SQLException e) {
-            throw new DBActionsException("problem adding a grade", e);
+            throw new DBActionsException("problem adding/retrieving course", e);
         } finally {
             dbAct.deleteGrade("snowboarding");
             courseDetails = null;
@@ -78,23 +76,8 @@ class ModelTest {
             rs = statement.executeQuery("SELECT count(*) FROM GPA WHERE course=('ski')");
             rs.next();
             assertEquals(0, rs.getInt(1));
-
         } catch (SQLException e) {
             throw new DBActionsException("problem deleting grade",e);
-        } finally {
-            if (statement != null) try {
-                statement.close();
-            } catch (SQLException e) {
-                throw new DBActionsException("problem closing Statement variable",e);
-            }
-            if (rs != null) try {
-                rs.close();
-            } catch (SQLException e) {
-                throw new DBActionsException("problem closing ResultSet variable",e);
-            }
-            conn = null;
-            rs = null;
-            statement = null;
         }
     }
 
@@ -110,50 +93,25 @@ class ModelTest {
 
         } catch (SQLException e) {
             throw new DBActionsException("problem updating the grade", e);
-        } finally {
-            dbAct.deleteGrade("scala");
-            if (statement != null) try {
-                statement.close();
-            } catch (SQLException e) {
-                throw new DBActionsException("problem closing Statement variable", e);
-            }
-            if (rs != null) try {
-                rs.close();
-            } catch (SQLException e) {
-                throw new DBActionsException("problem closing ResultSet variable", e);
-            }
         }
     }
 
     @org.junit.jupiter.api.Test
     void getCourse() throws DBActionsException {
-        CourseDetails newCourse=new CourseDetails("fifa",3,2,90,2.0,99);  // those param values will be compared in the assert methods ahead //
+        CourseDetails addedCourse = new CourseDetails("fifa",3,2,90,2.0,99); // those param values will be compared in the assert methods ahead //
+        CourseDetails courseDetails;
         try {
             statement = conn.createStatement();
-            dbAct.addGrade(newCourse);  //adding the newCourse to db
-            CourseDetails course=dbAct.getCourse("fifa");
-            assertEquals("fifa",course.getCourseName());
-            assertEquals(3,course.getYear());
-            assertEquals(2,course.getSemester());
-            assertEquals(90,course.getTestGrade());
-            assertEquals(2.0,course.getCredits());
-            assertEquals(99,course.getFinalGrade());
+            dbAct.addGrade(addedCourse);  //adding the newCourse to db
+            courseDetails=dbAct.getCourse("fifa");
+            assertTrue(courseDetails.isSameCourse(addedCourse));
 
         } catch (SQLException e) {
-            throw new DBActionsException("problem getting a course from db",e);
+            throw new DBActionsException("problem adding/retrieving course", e);
         }finally {
             dbAct.deleteGrade("fifa");
-            newCourse=null;
-            if (statement != null) try {
-                statement.close();
-            } catch (SQLException e) {
-                throw new DBActionsException("problem closing Statement variable",e);
-            }
-            if (rs != null) try {
-                rs.close();
-            } catch (SQLException e) {
-                throw new DBActionsException("problem closing ResultSet variable",e);
-            }
+            courseDetails=null;
+            addedCourse=null;
 
         }
     }
